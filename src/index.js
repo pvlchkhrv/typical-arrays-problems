da7af568b45b64f7faced8127e1ef13a9e6b6b78
pvl <pvlchkhrv@gmail.com>
2021-02-25 11:20:47 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (!array || array.length === 0) return 0;
+    return array.reduce((acc, cur) => acc > cur ? acc = cur : acc)
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!array || array.length === 0) return 0;
+    return array.reduce((acc, cur) => acc < cur ? acc = cur : acc)
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!array || array.length === 0) return 0;
+    return array.reduce((acc, cur) => acc + cur) / array.length
 }
